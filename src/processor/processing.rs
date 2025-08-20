273ee5ec1b78b0b8c1dbf29d658e90f1d89f23ed
Sofia Silvestri <ms.sophia.silvestri@gmail.com>
2025-08-20 13:42:18 +0200

    Added ProcessingBlockProcessor

--- src/processor/processing.rs
+++ src/processor/processing.rs
@@ -1,7 +1,7 @@
 use std::ffi::c_void;
 use std::collections::HashMap;
 use std::any::Any;
-
+use std::sync::mpsc::{Receiver, Sender, channel};
 use crate::processor::parameter::ParameterModel;
 
 pub trait ProcessingBlockTrait {
@@ -9,4 +9,15 @@ pub trait ProcessingBlockTrait {
     fn get_block_id(&self) -> u64;
     fn get_parameters_model(&self) -> HashMap<String, Box<dyn ParameterModel + Send>>;
     fn get_parameters_value(&self) -> HashMap<String, Box<dyn Any + Send>>;
+
+    fn get_input_number(&self) -> u32;
+    fn get_output_number(&self) -> u32;
+    fn get_input_type(&self, input_number: u32) -> Box<dyn Any + 'static>;
+    fn get_output_type(&self, output_number: u32) -> Box<dyn Any + 'static>;
+}
+
+pub struct ProcessingBlockProcessor {
+    processors: HashMap<u64, Box<dyn ProcessingBlockTrait>>,
+    input_receiver: Receiver<*const c_void>,
+    output_sender: Sender<*mut c_void>,
 }
